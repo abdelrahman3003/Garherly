5f968258741c07af3c6045cd5198f913c36373af
DESKTOP-LA7EI9C\abelrahamn <abdelrahmantemsah30@gmail.com>
2024-05-04 10:39:33 +0300

    gathery part2

--- lib/view/home/buttom_navigator_bar.dart
+++ lib/view/home/buttom_navigator_bar.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gatherly/controller/button_navigator_bar_controller.dart';
-import 'package:gatherly/core/constatnt/app_color.dart';
-import 'package:gatherly/view/home/widget/appButtonNavigarBar.dart';
+import 'package:gatherly/view/home/widget/bottom_navigate/appButtonNavigarBar.dart';
 import 'package:get/get.dart';
 
 class BottomNavigationScreen extends StatelessWidget {
@@ -11,30 +10,47 @@ class BottomNavigationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Get.put(ButtonNavigatorBarControllerImp());
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Event Details",
-            style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w700),
-          ),
-          centerTitle: true,
-          leading: const Icon(
-            Icons.arrow_back,
-          ),
-        ),
-        bottomNavigationBar: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: const AppBottomNavigatorBar(),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: FloatingActionButton(
-            backgroundColor: AppColor.primary,
-            onPressed: () {},
-            child: const Icon(Icons.home)),
-        body: GetBuilder<ButtonNavigatorBarControllerImp>(
-          builder: (controller) {
-            return SafeArea(child: controller.pageList[controller.pageCount]);
-          },
-        ));
+    return DefaultTabController(
+      length: 3,
+      child: GetBuilder<ButtonNavigatorBarControllerImp>(builder: (controller) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text(
+                  controller.appBarTilte(),
+                  style:
+                      TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w700),
+                ),
+                centerTitle: true,
+                leading: const Icon(
+                  Icons.arrow_back,
+                ),
+                actions: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.more_horiz),
+                  ),
+                ],
+                bottom: controller.isbar
+                    ? TabBar(tabs: [
+                        Tab(text: "tab1"),
+                        Tab(text: "tab2"),
+                        Tab(text: "tab3"),
+                      ])
+                    : PreferredSize(
+                        preferredSize: Size.zero, child: SizedBox())),
+            bottomNavigationBar: const AppBottomNavigatorBar(),
+            // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+            // floatingActionButton: FloatingActionButton(
+            //     backgroundColor: AppColor.primary,
+            //     onPressed: () {},
+            //     child: const Icon(Icons.home)),
+            body: GetBuilder<ButtonNavigatorBarControllerImp>(
+              builder: (controllerbuild) {
+                return SafeArea(
+                    child: controllerbuild.pageList[controllerbuild.pageCount]);
+              },
+            ));
+      }),
+    );
   }
 }
--- lib/view/home/chat_view.dart
+++ lib/view/home/chat_view.dart
@@ -5,6 +5,6 @@ class ChatView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Chat view"));
+    return const Center(child: Center(child: Text("data")));
   }
 }
